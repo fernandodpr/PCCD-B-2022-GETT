8305e183dac76850a36421996393bc7ff2abb92d
Fernando de Peroy Rodríguez <contacto@fernandodpr.es>
2023-02-07 10:40:37 +0000

    first commit

---
+++ Practica1/ej1.c
@@ -0,0 +1,6 @@
+//Ejercicio 1 de la práctica 1
+//Ejercicio 1 (mini ejemplos):
+//Consulte el man del comando kill.
+//Escriba un proceso p1 que espere a recibir una señal utilizando la función pause() (consulte el man de
+//dicha función). Envíe a dicho proceso, utilizando el comando kill, cada una de las señales definidas y
+//compruebe la acción por defecto de cada una de ellas.
